018119b64326c287ac3dd4ce76467adf26d3f899
Chris Jensen <chrisjensen3@mail.weber.edu>
2023-10-18 10:57:56 -0600

    Added table layout for students

---
+++ CASPARWeb/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPARWeb.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+		//Purposely left blank as Datatables will handle the data with js
+	}
+}
