0dbc665dfcb5f2069ad90db14e7cc0fa21a1e3aa
Jaeden Fisher <Javitar27@gmail.com>
2023-10-21 00:11:09 -0600

    Updated semester templates and added release times page.

---
+++ CASPARWeb/Pages/Administrator/ReleaseTimes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using DataAccess;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq.Expressions;
+
+namespace CASPARWeb.Pages.Administrator.ReleaseTimes
+{
+    public class IndexModel : PageModel
+    {
+    }
+}
--- CASPARWeb/wwwroot/js/semesters.js
+++ CASPARWeb/wwwroot/js/semesters.js
@@ -16,10 +16,10 @@ function loadList() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                    <a href="/Administrator/Templates/Index?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width: 100px;">
-                        <i class="far fa-edit"></i> Edit </a>
-                    <a href="/Administrator/Semesters/Delete?id=${data}" class="btn btn-danger text-white" style="cursor:pointer; width: 100px;">
-                        <i class="far fa-trash-alt"></i> Delete </a>
+                    <a href="/Administrator/Templates/Index?id=${data}" class="btn btn-outline-primary mt-1 rounded" style="cursor:pointer; style="cursor:pointer; width: 100px;">
+                        <i class="bi bi-pencil-square"></i> Edit </a>
+                    <a href="/Administrator/Semesters/Delete?id=${data}" class="btn btn-outline-danger mt-1 rounded" style="cursor:pointer; style="cursor:pointer; width: 100px;">
+                        <i class="bi bi-trash"></i> Delete </a>
                     </div>`;
                 }, "width": "30%"
             }
