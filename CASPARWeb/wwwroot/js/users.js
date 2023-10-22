83ed28ae5a4db8eccf5e20628dbf9b4f840c5f52
Jaeden Fisher <Javitar27@gmail.com>
2023-10-22 11:49:39 -0600

    Users (Administrator) CRUD

---
+++ CASPARWeb/wwwroot/js/users.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadList();
+});
+function loadList() {
+    dataTable = $('#DT_Users').DataTable({
+        "ajax": {
+            "url": "/api/user",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            //should not be capital
+            { "data": "userFirstName", "width": "20%" },
+            { "data": "userLastName", "width": "20%" },
+            { "data": "userEmail", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                    <a href="/Administrator/Users/Upsert?id=${data}" class="btn btn-outline-primary mt-1 rounded" style="cursor:pointer; style="cursor:pointer; width: 100px;">
+                        <i class="bi bi-pencil-square"></i> Edit </a>
+                    <a href="/Administrator/Users/Delete?id=${data}" class="btn btn-outline-danger mt-1 rounded" style="cursor:pointer; style="cursor:pointer; width: 100px;">
+                        <i class="bi bi-trash"></i> Delete </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%"
+    });
+}
