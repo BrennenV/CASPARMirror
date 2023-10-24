0d58cdc987b9fa1a2130b912733960e948d93f7f
BrennenV <brennenvanderpool@mail.weber.edu>
2023-10-24 13:39:01 -0500

    Added CRUD functionality for Section Statuses

---
+++ CASPARWeb/Pages/Administrator/SectionStatuses/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPARWeb.Pages.Administrator.SectionStatuses
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
