228922eca7b30116f663b8cdb01bdb13fbf0cdd5
Joseph Brower <61985669+josephbrower@users.noreply.github.com>
2023-10-24 14:16:55 -0600

    Added campus, building, amenity, and part of class CRUD

---
+++ CASPARWeb/wwwroot/js/classroomList.js
@@ -0,0 +1,30 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadList();
+});
+function loadList() {
+    dataTable = $('#DT_Classrooms').DataTable({
+        "ajax": {
+            "url": "/api/classroomList",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            //should not be capital
+            { "data": "classroomNumber", "width": "25%" },
+            { "data": "building.buildingName", "width": "25%" },
+            { "data": "building.campus.campusName", "width": "25%" },
+            {
+                "data": "id", "render": function (data) {
+                    return `<div class="text-center">
+                    <a href="/Administrator/Classrooms/CreateClassroom?id=${data}" type="button" class="btn btn-outline-primary mt-1 rounded" style="cursor:pointer; width: 100px;">
+                        <i class="bi bi-pencil-square"></i> Edit </a>`;
+                }, "width": "25%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%"
+    });
+}
