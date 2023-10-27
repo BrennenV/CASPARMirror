6ab4a4904341e96e66452984cbb49265d7eba3f8
BrennenV <brennenvanderpool@mail.weber.edu>
2023-10-27 13:54:56 -0500

    Added COURSE crud

---
+++ CASPARWeb/Pages/Administrator/Courses/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPARWeb.Pages.Administrator.Courses
+{
+    public class IndexModel : PageModel
+    {
+    }
+}
