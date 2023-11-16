770997b2c2864f948b74d84c4cf8a0222c7a0768
BrennenV <brennenvanderpool@mail.weber.edu>
2023-11-16 14:13:41 -0600

    Added restrictions based on roles to specific areas. Also added a few roles and nav bar hiding/showing based on roles.

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -11,5 +11,7 @@ namespace Utility
         public const string ADMIN_ROLE = "Admin";
         public const string INSTRUCTOR_ROLE = "Instructor";
         public const string STUDENT_ROLE = "Student";
+        public const string PROGRAM_COORDINATOR_ROLE = "Program Coordinator";
+        public const string SECRETARY_ROLE = "Secretary";
     }
 }
