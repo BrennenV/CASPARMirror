2c33c3cd10579afd3037b478e54d8c3b090e8f8f
Chris Jensen <chrisjensen3@mail.weber.edu>
2023-12-01 11:42:46 -0700

    Fixed some stuff in upsert

--- CASPARWeb/Areas/Coord/Pages/BuildSchedule/Upsert.cshtml.cs
+++ CASPARWeb/Areas/Coord/Pages/BuildSchedule/Upsert.cshtml.cs
@@ -1,16 +1,20 @@
 using DataAccess;
 using Infrastructure.Models;
+using Microsoft.AspNetCore.Hosting.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.AccessControl;
+using Utility;
 
 namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 {
     public class UpsertModel : PageModel
     {
         private readonly UnitOfWork _unitOfWork;
-        [BindProperty]
+		private readonly UserManager<ApplicationUser> _userManager;
+		[BindProperty]
         public CourseSection objCourseSection { get; set; }
 		public Course objCourse { get; set; }
         public AcademicProgram objAcademicProgram { get; set; }
@@ -19,6 +23,7 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
         public IEnumerable<SelectListItem> SemesterInstanceList { get; set; }
         public IEnumerable<SelectListItem> ApplicationUserList { get; set; }
         public IEnumerable<SelectListItem> ModalityList { get; set; }
+		public IEnumerable<Modality> StudentModalityList { get; set; }
         public IEnumerable<SelectListItem> ClassroomList { get; set; }
         public IEnumerable<SelectListItem> TimeBlockList { get; set; }
         public IEnumerable<SelectListItem> DaysOfWeekList { get; set; }
@@ -27,9 +32,23 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
         public IEnumerable<SelectListItem> PayOrganizationList { get; set; }
         public IEnumerable<SelectListItem> SectionStatusList { get; set; }
 
-        public UpsertModel(UnitOfWork unitOfWork)
+		//Start of Student Report------------------------------------------------------------------------------------------------------------------------------
+		public IEnumerable<WishlistCourse> WishlistCourseList { get; set; }
+		public int StudentCount { get; private set; }
+		public IEnumerable<WishlistModality> WishlistModalityList { get; set; }
+		public Dictionary<int, int> ModalityCount { get; private set; }
+		public IEnumerable<PartOfDay> PartOfDayList { get; set; }
+		public IEnumerable<WishlistPartOfDay> WishlistPartOfDayList { get; set; }
+		public Dictionary<int, int> PartOfDayCount { get; private set; }
+		public IEnumerable<Campus> CampusList { get; set; }
+		public IEnumerable<WishlistCampus> WishlistCampusList { get; set; }
+		public Dictionary<int, int> CampusCount { get; private set; }
+		//End of Student Report------------------------------------------------------------------------------------------------------------------------------
+
+		public UpsertModel(UnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
         {
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
             objCourseSection = new CourseSection();
             objCourse = new Course();
             objAcademicProgram = new AcademicProgram();
@@ -38,7 +57,7 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
             SemesterInstanceList = new List<SelectListItem>();
             ApplicationUserList = new List<SelectListItem>();
             ModalityList = new List<SelectListItem>();
-            ClassroomList = new List<SelectListItem>();
+			ClassroomList = new List<SelectListItem>();
             TimeBlockList = new List<SelectListItem>();
             DaysOfWeekList = new List<SelectListItem>();
             PartOfTermList = new List<SelectListItem>();
@@ -46,7 +65,7 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
             PayOrganizationList = new List<SelectListItem>();
             SectionStatusList = new List<SelectListItem>();
         }
-        public IActionResult OnGet(int courseSectionId, int semesterInstanceId)
+		public async Task<IActionResult> OnGet(int courseSectionId, int semesterInstanceId)
         {
             //Populate the foreign keys to avoid foreign key conflicts
             CourseList = _unitOfWork.Course.GetAll(c => c.IsArchived != true).Select(c => new SelectListItem { Text = c.CourseTitle, Value = c.Id.ToString() });
@@ -78,8 +97,108 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
             {
                 return NotFound();
             }
-            //Create mode
-            return Page();
+
+			//Start of Student Report------------------------------------------------------------------------------------------------------------------------------
+			//Get the course id from the course section id
+			var courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
+
+			//List all the modalities, part of days, and campuses
+			StudentModalityList = _unitOfWork.Modality.GetAll(c => c.IsArchived != true);
+			PartOfDayList = _unitOfWork.PartOfDay.GetAll(c => c.IsArchived != true);
+			CampusList = _unitOfWork.Campus.GetAll(c => c.IsArchived != true);
+
+			//List all the modalities, part of days, and campuses that the students have selected
+			WishlistCourseList = _unitOfWork.WishlistCourse.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.CourseId == courseId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistModalityList = _unitOfWork.WishlistModality.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistPartOfDayList = _unitOfWork.WishlistPartOfDay.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistCampusList = _unitOfWork.WishlistCampus.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+
+			//Count the number of courses that share the courseId where the user is a student
+			StudentCount = 0;
+
+			foreach (var user in WishlistCourseList)
+			{
+				var applicationUser = user.Wishlist.ApplicationUser;
+				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				{
+					// This user is a student. Count the student.
+					StudentCount++;
+				}
+			}
+
+			ModalityCount = new Dictionary<int, int>();
+
+			foreach (var user in WishlistModalityList)
+			{
+				var applicationUser = user.Wishlist.ApplicationUser;
+				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				{
+					// This user is a student. Count the modality id.
+					int modalityId = user.ModalityId;
+					if (ModalityCount.ContainsKey(modalityId))
+					{
+						ModalityCount[modalityId]++;
+					}
+					else
+					{
+						ModalityCount[modalityId] = 1;
+					}
+				}
+			}
+
+			// Sort the dictionary in descending order by value
+			ModalityCount = ModalityCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+			PartOfDayCount = new Dictionary<int, int>();
+
+			foreach (var user in WishlistPartOfDayList)
+			{
+				var applicationUser = user.Wishlist.ApplicationUser;
+				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				{
+					// This user is a student. Count the part of day id.
+					int partOfDayId = user.PartOfDayId;
+					if (PartOfDayCount.ContainsKey(partOfDayId))
+					{
+						PartOfDayCount[partOfDayId]++;
+					}
+					else
+					{
+						PartOfDayCount[partOfDayId] = 1;
+					}
+				}
+			}
+
+			// Sort the dictionary in descending order by value
+			PartOfDayCount = PartOfDayCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+			CampusCount = new Dictionary<int, int>();
+
+			foreach (var user in WishlistCampusList)
+			{
+				var applicationUser = user.Wishlist.ApplicationUser;
+				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				{
+					// This user is a student. Count the campus id.
+					int campusId = user.CampusId;
+					if (CampusCount.ContainsKey(campusId))
+					{
+						CampusCount[campusId]++;
+					}
+					else
+					{
+						CampusCount[campusId] = 1;
+					}
+				}
+			}
+
+			// Sort the dictionary in descending order by value
+			CampusCount = CampusCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+			//End of Student Report------------------------------------------------------------------------------------------------------------------------------
+
+			//Create mode
+			return Page();
         }
         public IActionResult OnPost()
         {
