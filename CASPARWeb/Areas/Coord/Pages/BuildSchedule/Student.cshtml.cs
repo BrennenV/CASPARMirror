ce2c08522c37575497234b89446384558dde986c
Chris Jensen <chrisjensen3@mail.weber.edu>
2023-12-01 09:46:30 -0700

    Ordered results by desc

--- CASPARWeb/Areas/Coord/Pages/BuildSchedule/Student.cshtml.cs
+++ CASPARWeb/Areas/Coord/Pages/BuildSchedule/Student.cshtml.cs
@@ -13,7 +13,7 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 		private readonly UnitOfWork _unitOfWork;
 		private readonly UserManager<ApplicationUser> _userManager;
 		//Start of Student Report------------------------------------------------------------------------------------------------------------------------------
-		public IEnumerable<Wishlist> WishlistList { get; set; }
+		public IEnumerable<WishlistCourse> WishlistCourseList { get; set; }
 		public int StudentCount { get; private set; }
 		public IEnumerable<Modality> ModalityList { get; set; }
 		public IEnumerable<WishlistModality> WishlistModalityList { get; set; }
@@ -36,25 +36,29 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 			courseSectionId = 1;
 			semesterInstanceId = 1;
 		//Start of Student Report------------------------------------------------------------------------------------------------------------------------------
+			//Get the course id from the course section id
+			var courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
+
 			//List all the modalities, part of days, and campuses
 			ModalityList = _unitOfWork.Modality.GetAll(c => c.IsArchived != true);
 			PartOfDayList = _unitOfWork.PartOfDay.GetAll(c => c.IsArchived != true);
 			CampusList = _unitOfWork.Campus.GetAll(c => c.IsArchived != true);
 
 			//List all the modalities, part of days, and campuses that the students have selected
-			WishlistModalityList = _unitOfWork.WishlistModality.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId, null, "Wishlist,Wishlist.ApplicationUser");
-			WishlistPartOfDayList = _unitOfWork.WishlistPartOfDay.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId, null, "Wishlist,Wishlist.ApplicationUser");
-			WishlistCampusList = _unitOfWork.WishlistCampus.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistCourseList = _unitOfWork.WishlistCourse.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.CourseId == courseId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistModalityList = _unitOfWork.WishlistModality.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistPartOfDayList = _unitOfWork.WishlistPartOfDay.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+			WishlistCampusList = _unitOfWork.WishlistCampus.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
 
-			//Count the number of wishlist where the user is a student
-			WishlistList = _unitOfWork.Wishlist.GetAll(w => w.SemesterInstanceId == semesterInstanceId, null, "ApplicationUser");
+			//Count the number of courses that share the courseId where the user is a student
 			StudentCount = 0;
 
-			foreach (var user in WishlistList)
+			foreach (var user in WishlistCourseList)
 			{
-				var applicationUser = user.ApplicationUser;
+				var applicationUser = user.Wishlist.ApplicationUser;
 				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
 				{
+					// This user is a student. Count the student.
 					StudentCount++;
 				}
 			}
@@ -79,6 +83,9 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 				}
 			}
 
+			// Sort the dictionary in descending order by value
+			ModalityCount = ModalityCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
 			PartOfDayCount = new Dictionary<int, int>();
 
 			foreach (var user in WishlistPartOfDayList)
@@ -99,6 +106,9 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 				}
 			}
 
+			// Sort the dictionary in descending order by value
+			PartOfDayCount = PartOfDayCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
 			CampusCount = new Dictionary<int, int>();
 
 			foreach (var user in WishlistCampusList)
@@ -118,6 +128,9 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 					}
 				}
 			}
+
+			// Sort the dictionary in descending order by value
+			CampusCount = CampusCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
 	//End of Student Report------------------------------------------------------------------------------------------------------------------------------
 			return Page();
