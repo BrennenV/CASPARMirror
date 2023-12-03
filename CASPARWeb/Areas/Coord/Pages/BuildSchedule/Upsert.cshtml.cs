ae0a3a5c926c4abc859c36a67a1ab66e0f73e4a1
Chris Jensen <chrisjensen3@mail.weber.edu>
2023-12-03 08:15:12 -0700

    Moved student and instructor into if statement

--- CASPARWeb/Areas/Coord/Pages/BuildSchedule/Upsert.cshtml.cs
+++ CASPARWeb/Areas/Coord/Pages/BuildSchedule/Upsert.cshtml.cs
@@ -124,190 +124,233 @@ namespace CASPARWeb.Areas.Coord.Pages.BuildSchedule
 
 			//Start of Student Report------------------------------------------------------------------------------------------------------------------------------
 			//Get the course id from the course section id
-			var courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
+			var courseId = 0;
 
-			//List all the modalities, part of days, and campuses
-			StudentModalityList = _unitOfWork.Modality.GetAll(c => c.IsArchived != true);
-			PartOfDayList = _unitOfWork.PartOfDay.GetAll(c => c.IsArchived != true);
-			CampusList = _unitOfWork.Campus.GetAll(c => c.IsArchived != true);
+			if (courseSectionId == 0)
+            {
+                courseId = 0;
+			}
+            else
+            {
+				courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
+				WishlistCourseList = _unitOfWork.WishlistCourse.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.CourseId == courseId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
 
-			//List all the modalities, part of days, and campuses that the students have selected
-			WishlistCourseList = _unitOfWork.WishlistCourse.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.CourseId == courseId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
-			WishlistModalityList = _unitOfWork.WishlistModality.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
-			WishlistPartOfDayList = _unitOfWork.WishlistPartOfDay.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
-			WishlistCampusList = _unitOfWork.WishlistCampus.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+				//List all the modalities, part of days, and campuses
+				StudentModalityList = _unitOfWork.Modality.GetAll(c => c.IsArchived != true);
+				PartOfDayList = _unitOfWork.PartOfDay.GetAll(c => c.IsArchived != true);
+				CampusList = _unitOfWork.Campus.GetAll(c => c.IsArchived != true);
 
-			//Count the number of courses that share the courseId where the user is a student
-			StudentCount = 0;
+				//List all the modalities, part of days, and campuses that the students have selected
+				WishlistModalityList = _unitOfWork.WishlistModality.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+				WishlistPartOfDayList = _unitOfWork.WishlistPartOfDay.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
+				WishlistCampusList = _unitOfWork.WishlistCampus.GetAll(w => w.Wishlist.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "Wishlist,Wishlist.ApplicationUser");
 
-			foreach (var user in WishlistCourseList)
-			{
-				var applicationUser = user.Wishlist.ApplicationUser;
-				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				//Count the number of courses that share the courseId where the user is a student
+				StudentCount = 0;
+
+				foreach (var user in WishlistCourseList)
 				{
-					// This user is a student. Count the student.
-					StudentCount++;
+					var applicationUser = user.Wishlist.ApplicationUser;
+					if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+					{
+						// This user is a student. Count the student.
+						StudentCount++;
+					}
 				}
+
+				ModalityCount = new Dictionary<int, int>();
+
+				foreach (var user in WishlistModalityList)
+				{
+					var applicationUser = user.Wishlist.ApplicationUser;
+					if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+					{
+						// This user is a student. Count the modality id.
+						int modalityId = user.ModalityId;
+						if (ModalityCount.ContainsKey(modalityId))
+						{
+							ModalityCount[modalityId]++;
+						}
+						else
+						{
+							ModalityCount[modalityId] = 1;
+						}
+					}
+				}
+
+				// Sort the dictionary in descending order by value
+				ModalityCount = ModalityCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+				PartOfDayCount = new Dictionary<int, int>();
+
+				foreach (var user in WishlistPartOfDayList)
+				{
+					var applicationUser = user.Wishlist.ApplicationUser;
+					if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+					{
+						// This user is a student. Count the part of day id.
+						int partOfDayId = user.PartOfDayId;
+						if (PartOfDayCount.ContainsKey(partOfDayId))
+						{
+							PartOfDayCount[partOfDayId]++;
+						}
+						else
+						{
+							PartOfDayCount[partOfDayId] = 1;
+						}
+					}
+				}
+
+				// Sort the dictionary in descending order by value
+				PartOfDayCount = PartOfDayCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+				CampusCount = new Dictionary<int, int>();
+
+				foreach (var user in WishlistCampusList)
+				{
+					var applicationUser = user.Wishlist.ApplicationUser;
+					if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+					{
+						// This user is a student. Count the campus id.
+						int campusId = user.CampusId;
+						if (CampusCount.ContainsKey(campusId))
+						{
+							CampusCount[campusId]++;
+						}
+						else
+						{
+							CampusCount[campusId] = 1;
+						}
+					}
+				}
+
+				// Sort the dictionary in descending order by value
+				CampusCount = CampusCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 			}
 
-			ModalityCount = new Dictionary<int, int>();
+			
 
-			foreach (var user in WishlistModalityList)
+			//End of Student Report------------------------------------------------------------------------------------------------------------------------------
+			//Start of instructor report---------------------------------------------------------------------------------
+
+
+			//Using courseSectionId and semesterInstanceId
+			//List instructors and ranking them from their wishlist rank
+			//and where their their load requirements has not been met.
+			//var courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
+			//Get wishlists table with applicationUser table appended to it
+			if (courseSectionId == 0)
 			{
-				var applicationUser = user.Wishlist.ApplicationUser;
-				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
+				courseId = 0;
+			}
+			else
+			{
+				Course tempCourse = _unitOfWork.Course.Get(c => c.Id == courseId && c.IsArchived != true);
+				IEnumerable<WishlistCourse> wishlistCourses = _unitOfWork.WishlistCourse.GetAll(w => w.CourseId == tempCourse.Id && w.IsArchived != true, null, "Course");
+
+				IEnumerable<Wishlist> wishlists = _unitOfWork.Wishlist.GetAll(w => w.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "ApplicationUser");
+
+				List<Wishlist> tempWishlist = new List<Wishlist>();
+				foreach (WishlistCourse wishlistCourse in wishlistCourses)
 				{
-					// This user is a student. Count the modality id.
-					int modalityId = user.ModalityId;
-					if (ModalityCount.ContainsKey(modalityId))
+					foreach (Wishlist wishlist in wishlists)
 					{
-						ModalityCount[modalityId]++;
+						if (wishlist.Id == wishlistCourse.WishlistId)
+						{
+							tempWishlist.Add(wishlist);
+							break;
+						}
+					}
+				}
+				//Filter out all students from wishlist table
+				IEnumerable<ReleaseTime> allInstructorsList = _unitOfWork.ReleaseTime.GetAll(c => c.SemesterInstanceId == semesterInstanceId && c.IsArchived != true, null, "ApplicationUser");
+				foreach (ReleaseTime instructor in allInstructorsList)
+				{
+					foreach (Wishlist wishlist in tempWishlist)
+					{
+						if (wishlist.ApplicationUser.Id == instructor.ApplicationUser.Id)
+						{
+							InstructorReport temp = new InstructorReport();
+							temp.wishlist = wishlist;
+							instructorReport.Add(temp);
+							break;
+						}
+					}
+				}
+				if (instructorReport.Count == 0)
+				{
+					/*There are NO instructor preferences*/
+				}
+				//Fill instructor report class
+				for (int i = 0; i < instructorReport.Count; i++)
+				{
+					//Get and calculate the LoadApplied/LoadTotal
+					instructorReport[i].realiseTime = _unitOfWork.ReleaseTime.Get(r => r.SemesterInstanceId == semesterInstanceId && r.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && r.IsArchived != true).ReleaseTimeAmount;
+					instructorReport[i].loadReqAmount = _unitOfWork.LoadReq.Get(l => l.SemesterInstanceId == semesterInstanceId && l.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && l.IsArchived != true).LoadReqAmount;
+					WishlistCourse tempWishCourses = _unitOfWork.WishlistCourse.Get(c => c.WishlistId == instructorReport[i].wishlist.Id && c.CourseId == tempCourse.Id && c.IsArchived != true);
+					IEnumerable<CourseSection> tempCourseSections = _unitOfWork.CourseSection.GetAll(c => c.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && c.IsArchived != true && c.SemesterInstanceId == semesterInstanceId, null, "Course");
+					if (tempCourseSections == null || tempCourseSections.Count() == 0) {/*PASS*/}
+					else
+					{
+						foreach (CourseSection tempCourseSection2 in tempCourseSections)
+						{
+							instructorReport[i].sumOfCourseLoads += tempCourseSection2.Course.CourseCreditHours;
+						}
+					}
+					instructorReport[i].totalLoadApplied = instructorReport[i].sumOfCourseLoads + instructorReport[i].realiseTime;
+					//Get the ranking
+					instructorReport[i].ranking = tempWishCourses.PreferenceRank;
+					//Get all the modalities
+					IEnumerable<WishlistModality> tempModalitys = _unitOfWork.WishlistModality.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "Modality");
+					if (tempModalitys == null || tempModalitys.Count() == 0)
+					{
+
 					}
 					else
 					{
-						ModalityCount[modalityId] = 1;
+						foreach (WishlistModality tempModality in tempModalitys)
+						{
+							String temp = tempModality.Modality.ModalityName;
+							instructorReport[i].modalityList.Add(temp);
+						}
 					}
-				}
-			}
-
-			// Sort the dictionary in descending order by value
-			ModalityCount = ModalityCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-
-			PartOfDayCount = new Dictionary<int, int>();
-
-			foreach (var user in WishlistPartOfDayList)
-			{
-				var applicationUser = user.Wishlist.ApplicationUser;
-				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
-				{
-					// This user is a student. Count the part of day id.
-					int partOfDayId = user.PartOfDayId;
-					if (PartOfDayCount.ContainsKey(partOfDayId))
+					//Get all locations
+					IEnumerable<WishlistCampus> tempCampuses = _unitOfWork.WishlistCampus.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "Campus");
+					if (tempCampuses == null || tempCampuses.Count() == 0)
 					{
-						PartOfDayCount[partOfDayId]++;
+
 					}
 					else
 					{
-						PartOfDayCount[partOfDayId] = 1;
+						foreach (WishlistCampus tempCampus in tempCampuses)
+						{
+							String temp = tempCampus.Campus.CampusName;
+							instructorReport[i].locationList.Add(temp);
+						}
 					}
-				}
-			}
-
-			// Sort the dictionary in descending order by value
-			PartOfDayCount = PartOfDayCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-
-			CampusCount = new Dictionary<int, int>();
-
-			foreach (var user in WishlistCampusList)
-			{
-				var applicationUser = user.Wishlist.ApplicationUser;
-				if (await _userManager.IsInRoleAsync(applicationUser, SD.STUDENT_ROLE))
-				{
-					// This user is a student. Count the campus id.
-					int campusId = user.CampusId;
-					if (CampusCount.ContainsKey(campusId))
+					//Get all time blocks
+					IEnumerable<WishlistTimeBlock> tempTimeBlocks = _unitOfWork.WishlistTimeBlock.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "TimeBlock");
+					if (tempTimeBlocks == null || tempTimeBlocks.Count() == 0)
 					{
-						CampusCount[campusId]++;
+
 					}
 					else
 					{
-						CampusCount[campusId] = 1;
+						foreach (WishlistTimeBlock tempTimeBlock in tempTimeBlocks)
+						{
+							String temp = tempTimeBlock.TimeBlock.TimeBlockValue;
+							instructorReport[i].timeList.Add(temp);
+						}
 					}
 				}
+				//Rank instructor report items
+				instructorReport = instructorReport.OrderBy(i => i.ranking).ToList();
+
+				//Get semester name for report title
+				SemesterName = _unitOfWork.SemesterInstance.Get(s => s.Id == semesterInstanceId && s.IsArchived != true).SemesterInstanceName;
 			}
 
-			// Sort the dictionary in descending order by value
-			CampusCount = CampusCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-
-            //End of Student Report------------------------------------------------------------------------------------------------------------------------------
-            //Start of instructor report---------------------------------------------------------------------------------
-
-
-            //Using courseSectionId and semesterInstanceId
-            //List instructors and ranking them from their wishlist rank
-            //and where their their load requirements has not been met.
-            //var courseId = _unitOfWork.CourseSection.GetById(courseSectionId).CourseId;
-            //Get wishlists table with applicationUser table appended to it
-            Course tempCourse = _unitOfWork.Course.Get(c => c.Id == courseId && c.IsArchived != true);
-
-            IEnumerable<WishlistCourse> wishlistCourses = _unitOfWork.WishlistCourse.GetAll(w => w.CourseId == tempCourse.Id && w.IsArchived != true, null, "Course");
-            IEnumerable<Wishlist> wishlists = _unitOfWork.Wishlist.GetAll(w => w.SemesterInstanceId == semesterInstanceId && w.IsArchived != true, null, "ApplicationUser");
-
-            List<Wishlist> tempWishlist = new List<Wishlist>();
-            foreach (WishlistCourse wishlistCourse in wishlistCourses) {
-                foreach (Wishlist wishlist in wishlists) {
-                    if (wishlist.Id == wishlistCourse.WishlistId) {
-                        tempWishlist.Add(wishlist);
-                        break;
-                    }
-                }
-            }
-            //Filter out all students from wishlist table
-            IEnumerable<ReleaseTime> allInstructorsList = _unitOfWork.ReleaseTime.GetAll(c => c.SemesterInstanceId == semesterInstanceId && c.IsArchived != true, null, "ApplicationUser");
-            foreach (ReleaseTime instructor in allInstructorsList) {
-                foreach (Wishlist wishlist in tempWishlist) {
-                    if (wishlist.ApplicationUser.Id == instructor.ApplicationUser.Id) {
-                        InstructorReport temp = new InstructorReport();
-                        temp.wishlist = wishlist;
-                        instructorReport.Add(temp);
-                        break;
-                    }
-                }
-            }
-            if (instructorReport.Count == 0) {
-                /*There are NO instructor preferences*/
-            }
-            //Fill instructor report class
-            for (int i = 0; i < instructorReport.Count; i++) {
-                //Get and calculate the LoadApplied/LoadTotal
-                instructorReport[i].realiseTime = _unitOfWork.ReleaseTime.Get(r => r.SemesterInstanceId == semesterInstanceId && r.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && r.IsArchived != true).ReleaseTimeAmount;
-                instructorReport[i].loadReqAmount = _unitOfWork.LoadReq.Get(l => l.SemesterInstanceId == semesterInstanceId && l.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && l.IsArchived != true).LoadReqAmount;
-                WishlistCourse tempWishCourses = _unitOfWork.WishlistCourse.Get(c => c.WishlistId == instructorReport[i].wishlist.Id && c.CourseId == tempCourse.Id && c.IsArchived != true);
-                IEnumerable<CourseSection> tempCourseSections = _unitOfWork.CourseSection.GetAll(c => c.InstructorId == instructorReport[i].wishlist.ApplicationUser.Id && c.IsArchived != true && c.SemesterInstanceId == semesterInstanceId, null, "Course");
-                if (tempCourseSections == null || tempCourseSections.Count() == 0) {/*PASS*/} else {
-                    foreach (CourseSection tempCourseSection2 in tempCourseSections) {
-                        instructorReport[i].sumOfCourseLoads += tempCourseSection2.Course.CourseCreditHours;
-                    }
-                }
-                instructorReport[i].totalLoadApplied = instructorReport[i].sumOfCourseLoads + instructorReport[i].realiseTime;
-                //Get the ranking
-                instructorReport[i].ranking = tempWishCourses.PreferenceRank;
-                //Get all the modalities
-                IEnumerable<WishlistModality> tempModalitys = _unitOfWork.WishlistModality.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "Modality");
-                if (tempModalitys == null || tempModalitys.Count() == 0) {
-
-                } else {
-                    foreach (WishlistModality tempModality in tempModalitys) {
-                        String temp = tempModality.Modality.ModalityName;
-                        instructorReport[i].modalityList.Add(temp);
-                    }
-                }
-                //Get all locations
-                IEnumerable<WishlistCampus> tempCampuses = _unitOfWork.WishlistCampus.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "Campus");
-                if (tempCampuses == null || tempCampuses.Count() == 0) {
-
-                } else {
-                    foreach (WishlistCampus tempCampus in tempCampuses) {
-                        String temp = tempCampus.Campus.CampusName;
-                        instructorReport[i].locationList.Add(temp);
-                    }
-                }
-                //Get all time blocks
-                IEnumerable<WishlistTimeBlock> tempTimeBlocks = _unitOfWork.WishlistTimeBlock.GetAll(w => w.WishlistId == instructorReport[i].wishlist.Id && w.IsArchived != true, null, "TimeBlock");
-                if (tempTimeBlocks == null || tempTimeBlocks.Count() == 0) {
-
-                } else {
-                    foreach (WishlistTimeBlock tempTimeBlock in tempTimeBlocks) {
-                        String temp = tempTimeBlock.TimeBlock.TimeBlockValue;
-                        instructorReport[i].timeList.Add(temp);
-                    }
-                }
-            }
-            //Rank instructor report items
-            instructorReport = instructorReport.OrderBy(i => i.ranking).ToList();
-
-            //Get semester name for report title
-            SemesterName = _unitOfWork.SemesterInstance.Get(s => s.Id == semesterInstanceId && s.IsArchived != true).SemesterInstanceName;
+            
             //End of instructor report---------------------------------------------------------------------------------
 
 
